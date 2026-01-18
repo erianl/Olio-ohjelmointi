2d5d38238cf88e0019f5ff4b05f23dcab3d2fd5f
erianl <erian.l.rauf@gmail.com>
2026-01-18 22:04:45 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,44 +1,81 @@
+#include "olio1.h"
 #include <iostream>
-#include <cstdlib>
-#include <ctime>
 using namespace std;
 
-
-int game(int maxnum);
-
-int main() {
-    srand(time(NULL));
-
-    int maxnum = 40;
-    int arvaustenmaara = game(maxnum);
-
-
-
-    cout << "Arvausten maara: " << arvaustenmaara << endl;
-
-    return 0;
+GameNew::GameNew(int mN)
+{
+    cout << "Peli luotu " << endl;
+    maxNumber = mN;
 }
 
-int game(int maxnum) {
-    int arvottu = rand() % maxnum + 1;
-    int arvaus;
-    int laskuri = 0;
+GameNew::~GameNew()
+{
+    cout << "Peli tuhottu " << endl;
+}
 
-    while (true) {
-        cout << "Arvaa luku (1-" << maxnum << "): ";
-        cin >> arvaus;
+void GameNew::play()
+{
+    cout << "Pelissa " << endl;
 
-        laskuri++;
+    srand(time(NULL));
+    randomNumber = (rand() % maxNumber)+1;
 
-        if (arvaus < arvottu) {
-            cout << "Luku on suurempi." << endl;
-        } else if (arvaus > arvottu) {
-            cout << "Luku on pienempi." << endl;
-        } else {
-            cout << "Oikea vastaus!" << endl;
-            break;
+    while (playerGuess != randomNumber) {
+        cout << "Anna arvaus? " << endl;
+        cin >> playerGuess;
+
+        numOfGuesses++;
+
+
+        if (playerGuess == randomNumber) {
+            cout << "Oikein!!" << endl;
+        } else if (playerGuess < randomNumber){
+            cout << "Luku on suurempi!" << endl;
+        } else if (playerGuess > randomNumber) {
+            cout << "Luku on pienempi!" << endl;
         }
     }
 
-    return laskuri;
+}
+
+void GameNew::printGameResult()
+{
+    cout << "Tulostuksessa " << endl;
+    cout << "Oikea luku: " << randomNumber << endl << "Arvausten maara: " << numOfGuesses << endl;
+}
+
+
+#ifndef GAMENEW_H
+#define GAMENEW_H
+
+class GameNew
+{
+public:
+    GameNew(int);
+    ~GameNew();
+    void play();
+    void printGameResult();
+private:
+    int maxNumber = 0;
+    int playerGuess = 0;
+    int randomNumber = 0;
+    int numOfGuesses = 0;
+protected:
+
+};
+
+#endif
+
+
+#include "gamenew.h"
+#include <iostream>
+
+int main(){
+    int maxNum(50);
+    GameNew peliOlio(maxNum);
+    peliOlio.play();
+    peliOlio.printGameResult();
+
+    std::cout << "Arvasit oikein" << std::endl;
+    return 0;
 }
